f3b0ee725151f4bd16c88c03fc5378ee474ed882
Kishan Suvarna <kishansuvarna09@gmail.com>
2025-05-16 01:24:26 +0400

    chore: updated traefik configuration

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const withNextra = nextra({});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: "standalone"
 };
 
 export default withNextra(nextConfig);
